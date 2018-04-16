c8849895c0acfcdef9067e8d32f3b2b1730d49be
Aksenovskiy_Anton <axelgong@gmail.com>
2018-04-16 15:18:26 +0300

    complex

--- src/Complex.java
+++ src/Complex.java
@@ -1,2 +1,45 @@
 public class Complex {
+    private double dReal, dImaginary;
+
+    public Complex() {
+    }
+
+    public Complex(double dReal, double dImaginary) {
+        this.dReal = dReal;
+        this.dImaginary = dImaginary;
+    }
+
+    public String toString() {
+        if (dImaginary >= 0)
+            return dReal + " + " + dImaginary + "i";
+        else
+            return dReal + " - " + -dImaginary + "i";
+    }
+
+    // sum of two complex numbers cA, cB
+
+    public Complex Add(Complex complexB) {
+        Complex sum = new Complex();
+        sum.dReal = dReal + complexB.dReal;
+        sum.dImaginary = dImaginary + complexB.dImaginary;
+        return sum;
+    }
+
+    // difference of two complex numbers cA, cB
+
+    public Complex Sub(Complex complexB) {
+        Complex diff = new Complex();
+        diff.dReal = dReal - complexB.dReal;
+        diff.dImaginary = dImaginary - complexB.dImaginary;
+        return diff;
+    }
+
+    // composition of two complex numbers cA, cB
+
+    public Complex Multi(Complex complexB) {
+        Complex prod = new Complex();
+        prod.dReal = dReal * complexB.dReal - dImaginary * complexB.dImaginary;
+        prod.dImaginary = dImaginary * complexB.dReal + dReal * complexB.dImaginary;
+        return prod;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("12");
+    public static Complex complexA;
+    public static Complex complexB;
+
+    public static void addComplex() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("ВВедите действительную и мнимую часть комплексного числа 1");
+        complexA = new Complex(in.nextDouble(), in.nextDouble());
+        System.out.println("ВВедите действительную и мнимую часть комплексного числа 2");
+        complexB = new Complex(in.nextDouble(), in.nextDouble());
+    }
+
+    public static void main(String args[]) {
+        addComplex();
+        System.out.println("complex_1 = " + complexA.toString());
+        System.out.println("complex_2 = " + complexB.toString());
+
+        Complex cSum = complexA.Add(complexB);
+        System.out.println("complex_1 + complex_2 = " + cSum.toString());
+
+        Complex cDiff = complexA.Sub(complexB);
+        System.out.println("complex_1 - complex_2 = " + cDiff.toString());
+
+        Complex cMulti = complexA.Multi(complexB);
+        System.out.println("complex_1 * complex_2 = " + cMulti.toString());
     }
 }
